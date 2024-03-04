bae6372ed7723dd340202c146666f4e159adb97e
Valery Zonov <valery.zonov@sibers.com>
2024-03-04 16:58:03 +0700

    sample routing

--- index.js
+++ index.js
@@ -4,9 +4,23 @@ const port = 8001;
 
 const app = express();
 
-app.get('/hello', (req,res) => {
-	res.send('Hello!')
+app.all('/hello', (req, res, next) => {
+	console.log('All');
+	next()
 })
+
+const cb = (req, res, next) => {
+	console.log('CB')
+	next()
+}
+
+app.route('/user')
+	.get('/hello', cb, (req, res) => {
+		res.send('Hello!');
+	})
+	.post('/hello', cb, (req, res) => {
+		res.send('Hello POST!');
+	});
 
 app.listen(port, () => {
 	console.log(`Server start : http://localhost:${port}`);
