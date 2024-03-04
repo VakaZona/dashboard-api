e7065079c3395bd038c8e004d699472e64804f3e
Valery Zonov <valery.zonov@sibers.com>
2024-03-04 17:22:37 +0700

    sample response

--- index.js
+++ index.js
@@ -4,23 +4,10 @@ const port = 8001;
 
 const app = express();
 
-app.all('/hello', (req, res, next) => {
-	console.log('All');
-	next()
-})
-
-const cb = (req, res, next) => {
-	console.log('CB')
-	next()
-}
-
-app.route('/user')
-	.get('/hello', cb, (req, res) => {
-		res.send('Hello!');
-	})
-	.post('/hello', cb, (req, res) => {
-		res.send('Hello POST!');
-	});
+app.get('/hello', (req, res) => {
+	res.status(404);
+	res.end()
+});
 
 app.listen(port, () => {
 	console.log(`Server start : http://localhost:${port}`);
