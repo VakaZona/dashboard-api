5ab681b2ae8902bd742502a510e0f6d3633564a1
Valery Zonov <valery.zonov@sibers.com>
2024-03-07 16:44:50 +0700

    add logger

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import express, { Express } from 'express'
 import { userRouter } from './users/users'
-import {Server} from 'http'
+import { Server } from 'http'
+import { LoggerService } from './logger/logger.service'
 
 export class App {
 	app: Express
 	server: Server
 	port: number
-	constructor() {
+	logger: LoggerService
+
+	constructor(logger: LoggerService) {
 		this.app = express()
 		this.port = 8000
+		this.logger = logger
 	}
 
 	useRoutes() {
@@ -18,7 +22,7 @@ export class App {
 	public async init() {
 		this.useRoutes()
 		this.server = this.app.listen(this.port)
-		console.log(`Server start : http://localhost:${this.port}`);
+		this.logger.log(`Server start : http://localhost:${this.port}`)
 
 	}
 }
