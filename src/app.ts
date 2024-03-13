ac06b9de49c3923eee950237335b0ab0f5242ab3
Valery Zonov <valery.zonov@sibers.com>
2024-03-13 11:44:09 +0700

    test code formatter

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,29 @@
-import express, { Express } from 'express'
-import { Server } from 'http'
-import { LoggerService } from './logger/logger.service'
-import { UserController } from './users/users.controller'
+import express, { Express } from "express";
+import { Server } from "http";
+import { LoggerService } from "./logger/logger.service";
+import { UserController } from "./users/users.controller";
 
 export class App {
-	app: Express
-	server: Server
-	port: number
-	logger: LoggerService
-	userController: UserController
+  app: Express;
+  server: Server;
+  port: number;
+  logger: LoggerService;
+  userController: UserController;
 
-	constructor(
-		logger: LoggerService,
-		userController: UserController
-	) {
-		this.app = express()
-		this.port = 8000
-		this.logger = logger
-		this.userController = userController
-	}
+  constructor(logger: LoggerService, userController: UserController) {
+    this.app = express();
+    this.port = 8000;
+    this.logger = logger;
+    this.userController = userController;
+  }
 
-	useRoutes() {
-		this.app.use('/users', this.userController.router)
-	}
+  useRoutes() {
+    this.app.use("/users", this.userController.router);
+  }
 
-	public async init() {
-		this.useRoutes()
-		this.server = this.app.listen(this.port)
-		this.logger.log(`Server start : http://localhost:${this.port}`)
-
-	}
+  public async init() {
+    this.useRoutes();
+    this.server = this.app.listen(this.port);
+    this.logger.log(`Server start : http://localhost:${this.port}`);
+  }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,21 +1,21 @@
-import { NextFunction, Request, Response } from 'express';
-import { BaseController } from '../common/base.controller';
-import { LoggerService } from '../logger/logger.service';
+import { NextFunction, Request, Response } from "express";
+import { BaseController } from "../common/base.controller";
+import { LoggerService } from "../logger/logger.service";
 
 export class UserController extends BaseController {
-	constructor(logger: LoggerService) {
-		super(logger)
-		this.bindRoutes([
-			{ path: '/register', method: 'post', func: this.register},
-			{ path: '/login', method: 'post', func: this.login},
-		])
-	}
+  constructor(logger: LoggerService) {
+    super(logger);
+    this.bindRoutes([
+      { path: "/register", method: "post", func: this.register },
+      { path: "/login", method: "post", func: this.login },
+    ]);
+  }
 
-	login(req: Request, res:Response, next: NextFunction) {
-		this.ok(res, 'login')
-	}
+  login(req: Request, res: Response, next: NextFunction) {
+    this.ok(res, "login");
+  }
 
-	register(req: Request, res: Response, next: NextFunction) {
-		this.ok(res, 'register')
-	}
+  register(req: Request, res: Response, next: NextFunction) {
+    this.ok(res, "register");
+  }
 }
