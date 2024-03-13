8437bd83ea5c628b4265c9856faeb5e019dee199
Valery Zonov <valery.zonov@sibers.com>
2024-03-13 14:42:33 +0700

    eslint and prettier

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,29 +1,29 @@
-import { Logger } from "tslog";
-import { ILogger } from "./logger.interface";
-import { injectable } from "inversify";
-import "reflect-metadata";
+import { Logger } from 'tslog';
+import { ILogger } from './logger.interface';
+import { injectable } from 'inversify';
+import 'reflect-metadata';
 
 @injectable()
 export class LoggerService implements ILogger {
-  public logger;
-  constructor() {
-    this.logger = new Logger({
-      // displayInstanceName: false,
-      // displayLoggerName: false,
-      // displayFilePath: 'hidden',
-      // displayFunctionName: false
-    });
-  }
+	public logger;
+	constructor() {
+		this.logger = new Logger({
+			// displayInstanceName: false,
+			// displayLoggerName: false,
+			// displayFilePath: 'hidden',
+			// displayFunctionName: false
+		});
+	}
 
-  log(...args: unknown[]) {
-    this.logger.info(...args);
-  }
+	log(...args: unknown[]): void {
+		this.logger.info(...args);
+	}
 
-  error(...args: unknown[]) {
-    this.logger.error(...args);
-  }
+	error(...args: unknown[]): void {
+		this.logger.error(...args);
+	}
 
-  warn(...args: unknown[]) {
-    this.logger.warn(...args);
-  }
+	warn(...args: unknown[]): void {
+		this.logger.warn(...args);
+	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,30 @@
-import { Container, ContainerModule, interfaces } from "inversify";
-import { App } from "./app";
-import { ExeptionFilter } from "./errors/exeption.filter";
-import { LoggerService } from "./logger/logger.service";
-import { UserController } from "./users/users.controller";
-import { ILogger } from "./logger/logger.interface";
-import { TYPES } from "./types";
-import { IExeptionFilter } from "./errors/exeption.filter.interface";
+import { Container, ContainerModule, interfaces } from 'inversify';
+import { App } from './app';
+import { ExeptionFilter } from './errors/exeption.filter';
+import { LoggerService } from './logger/logger.service';
+import { UserController } from './users/users.controller';
+import { ILogger } from './logger/logger.interface';
+import { TYPES } from './types';
+import { IExeptionFilter } from './errors/exeption.filter.interface';
+
+export interface IBootstrapReturnType {
+	appContainer: Container;
+	app: App;
+}
 
 export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
-  bind<ILogger>(TYPES.ILogger).to(LoggerService);
-  bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
-  bind<UserController>(TYPES.UserController).to(UserController);
-  bind<App>(TYPES.Application).to(App);
+	bind<ILogger>(TYPES.ILogger).to(LoggerService);
+	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
+	bind<UserController>(TYPES.UserController).to(UserController);
+	bind<App>(TYPES.Application).to(App);
 });
 
-function bootstrap() {
-  const appContainer = new Container();
-  appContainer.load(appBindings);
-  const app = appContainer.get<App>(TYPES.Application);
-  app.init();
-  return { appContainer, app };
+function bootstrap(): IBootstrapReturnType {
+	const appContainer = new Container();
+	appContainer.load(appBindings);
+	const app = appContainer.get<App>(TYPES.Application);
+	app.init();
+	return { appContainer, app };
 }
 
 export const { app, appContainer } = bootstrap();
