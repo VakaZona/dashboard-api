caecb705b21db7e89c43ace4c2cac419ee135cc8
Valery Zonov <valery.zonov@sibers.com>
2024-03-13 12:39:51 +0700

    test DI pattern

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,25 +1,26 @@
-import { Logger } from "tslog"
+import { Logger } from "tslog";
+import { ILogger } from "./logger.interface";
 
-export class LoggerService {
-	public logger
-	constructor() {
-		this.logger = new Logger({
-			// displayInstanceName: false,
-			// displayLoggerName: false,
-			// displayFilePath: 'hidden',
-			// displayFunctionName: false
-		})
-	}
+export class LoggerService implements ILogger {
+  public logger;
+  constructor() {
+    this.logger = new Logger({
+      // displayInstanceName: false,
+      // displayLoggerName: false,
+      // displayFilePath: 'hidden',
+      // displayFunctionName: false
+    });
+  }
 
-	log(...args: unknown[]) {
-		this.logger.info(...args)
-	}
+  log(...args: unknown[]) {
+    this.logger.info(...args);
+  }
 
-	error(...args: unknown[]) {
-		this.logger.error(...args)
-	}
+  error(...args: unknown[]) {
+    this.logger.error(...args);
+  }
 
-	warn(...args: unknown[]) {
-		this.logger.warn(...args)
-	}
+  warn(...args: unknown[]) {
+    this.logger.warn(...args);
+  }
 }
